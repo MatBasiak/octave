aa9d64afe24916a5e84381c4e7aca4639471da13
Mateusz Basiak <mateusz.basiak@gmail.com>
2022-06-19 15:19:38 +0200

    Add files via upload

--- gradient.m
+++ gradient.m
@@ -1,12 +1,13 @@
-punkt_startowy = rand()-0.5
+punkt_startowy = rand()*1.5*pi +pi/2;
 wartosc_w_punkcie_startowym = sin(punkt_startowy);
 punkt = punkt_startowy
-wsp_uczenia = 0.001
+wsp_uczenia = 0.01
 
 for i=1:300
- gradient = 3*punkt^2 +2*punkt;
+ gradient = cos(punkt);
  punkt -= wsp_uczenia *gradient; 
- 
+ sin(punkt) ;
 end
 
+idealna_wartosc= 3*pi/2
 punkt
